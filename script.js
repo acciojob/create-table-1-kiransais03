6375544ee8b8a24c8aee3fedf1c72a348abb932f
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-26 18:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  let table=document.getElementById('sampleTable');
+	let row=table.insertRow(0);
+	row.innerHTML=`<td>New Cell1</td>
+                  <td>New Cell2</td>`
 }
